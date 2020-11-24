0d8256ce6ee7e092ab4ff827eecdaf87d649c454
utgwkk <utagawakiki@gmail.com>
2020-11-24 21:27:39 +0900

    breaking fizzbuzz program

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ def main(argv):
 def process(filename):
     tokenized = tokenize.generate_tokens(tokenize.open(filename).readline)
     tokens_after = []
-    
+
     for token in tokenized:
         if token.type == tokenize.NUMBER:
             new_number = int(token.string) * 2
-            tokens_after.append((tokenize.NUMBER, str(new_number)))
+            tokens_after.append((tokenize.NUMBER, str(new_number), token.start, token.end, token.line))
         else:
-            tokens_after.append((token.type, token.string))
-    
-    print(tokenize.untokenize(tokens_after))
+            tokens_after.append(token)
+
+    with open(filename, 'wt') as f_out:
+        f_out.write(tokenize.untokenize(tokens_after))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
